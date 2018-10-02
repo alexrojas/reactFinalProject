8119ad921bebee80b9b4fac37ee29a12263792d0
alexrojas <alex.rojas@agintechllc.com>
2018-10-02 15:54:02 -0400

    model profile done

--- models/Profile.js
+++ models/Profile.js
@@ -3,11 +3,29 @@ const Schema = mongoose.Schema
 
 //Create Schema
 const ProfileSchema = new Schema({
-  user:    {type: Schema.Types.ObjectId, ref: 'users'  },
-  car:     {type: String},
-  address: {type: String, required: true},
-  status:  {type: String, required: true}
-  message: {type: String}, //i have to ask about this to NIck
+  user:          {type: Schema.Types.ObjectId, ref: 'users'},
+  car:           {type: String},
+  address:       {type: String, required: true},
+  status:        {type: String, required: true}
+  message:       {type: String}, //i have to ask about this to NIck
+  bio:           {type: String},
+  referralCode:  {type: String},
+  social:        {
+    youtube: {
+      type: String
+    },
+    twitter: {
+      type: String
+    },
+    facebook: {
+      type: String
+    },
+    instagram: {
+      type: String
+    },
+  },
+  date: {type: Date, defaul: Date.now},
+
 })
 
 const Profile = mongoose.model("Profile", ProfileSchema);
